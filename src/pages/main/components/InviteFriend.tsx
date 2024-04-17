50df2072882818b36a7604728b41f09c7c00753c
magalutfullaev <mhoja9494@gmail.com>
2024-04-17 18:59:13 +0500

    remove line from bottom

--- src/pages/main/components/InviteFriend.tsx
+++ src/pages/main/components/InviteFriend.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
 import css from './InviteFriend.module.scss'
-import {Link} from "react-router-dom";
-import {useTelegram} from "@/utils/hooks/useTelegram";
-import axios from "@/axios";
-import {UserGet, UserGetResponse} from "@/utils/types";
+import {AuthResponse, AuthUser} from "@/utils/types";
 import {useSelector} from "react-redux";
 
 const InviteFriend = ({ closeModal }: any) => {
-  const currentUser: UserGet = useSelector((state: UserGetResponse) => state.currentUser);
+  const authUser: AuthUser = useSelector((state: AuthResponse) => state.auth);
 
   return (
     <div className={css.inviteFriend} onClick={closeModal}>
       <div className={css.inviteContent} onClick={(e) => e.stopPropagation()}>
         <p className={css.inviteTitle}>Чтобы запустить трекер воды вам нужно пригласить хотя бы одного друга</p>
         <p className={css.inviteText}>Нажми на кнопку ниже</p>
-        <a href={`https://t.me/share/url?url=Привет,+я+слежу+за+своим+уровнем+воды+прямо+в+Telegram%0A%0AХочешь+также?+Тогда+переходи+по+ссылке:+https://t.me/imlegendbot?start=${currentUser.data.user_id}`} className={css.inviteBtn}>
+        <a href={`https://t.me/share/url?url=Привет,+я+слежу+за+своим+уровнем+воды+прямо+в+Telegram%0A%0AХочешь+также?+Тогда+переходи+по+ссылке:+https://t.me/imlegendbot?start=${authUser.user[0].user_id}`} className={css.inviteBtn}>
           <div className={css.inviteBtnText}>Пригласить друга</div>
         </a>
         <a href="#" onClick={closeModal} className={css.inviteBtn}>
