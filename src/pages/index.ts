be65174bb561f3b0f5f39ea0c8c7561b0a941097
tvdmitry <dmitry.tulupov68@gmail.com>
2024-09-16 16:58:39 +0300

    fix: fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 import { lazy } from 'react';
+
 const DeliveryPage = lazy(() => import('./delivery/DeliveryPage'));
 const MainPage = lazy(() => import('./main/MainPage'));
 const ManualsPage = lazy(() => import('./manuals/ManualsPage'));
